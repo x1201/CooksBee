1e3853faa2c664dfb44852fe63343fd4df4f39a8
park9 <park9704271@naver.com>
2022-04-15 14:35:04 +0900

    MainActivity.kt , SearchActivity.kt, SearchAdapter.kt 코드 수정 검색알고리즘 SearchAdapter.kt의 filter에서 진행

--- app/src/main/java/com/example/capstone/MainActivity.kt
+++ app/src/main/java/com/example/capstone/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.capstone
 
 import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 import com.example.capstone.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -14,57 +16,39 @@ class MainActivity : AppCompatActivity() {
     private var code : String ="ixpayhTvaQr46BKBbLFk"
 
     var db = FirebaseFirestore.getInstance()
-    var firstNameList: ArrayList<NameInfo>? = null // id, name, 재료를 넣은 리스트
-    lateinit var firstSearchList : ArrayList<NameInfo> // 검색결과를 담을 리스트 선언
-
+    var DBLists: ArrayList<NameInfo>? = null // id, name, 재료를 넣은 리스트
+    lateinit var adapterCatchLists : ArrayList<NameInfo> //searchAdapter에서 리스트 받아오는 리스트
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
         binding.startbtn.setOnClickListener {
             val intent = Intent(this, UtubeSearchActivity::class.java)
-           // intent.putExtra("code", code)
+            //intent.putExtra("code", code)
             startActivity(intent)
         }
-
         var nextIntent = Intent(this, SearchActivity::class.java)
 
         binding.MainSearchButton.setOnClickListener(View.OnClickListener {
-            firstSearchList.clear()
             var searchText = binding.MainSearchText.text.toString()
-            for(i in 0 until firstNameList?.size!!){
-                if(searchText.equals(null)){
-                    Log.d(ContentValues.TAG,"null")
-                    break
-                }
-                else if(firstNameList!![i].name.contains(searchText)){ //이름으로 검색
-                    firstSearchList?.add(NameInfo(firstNameList!![i].id,firstNameList!![i].name,firstNameList!![i].ingredient,firstNameList!![i].url))
-                    //이 값을 intent를 사용해 SearchActivity로 전달해 onCreate할 때 띄워져서 보여지게 해야함
-                    //intent에 데이터를 넣는법 = nextIntent.putExtra(String name, Serializable value)
-                    nextIntent.putExtra("firstSearchList", firstSearchList)
-                }
-            }
-            startActivity(nextIntent) //검색 버튼을 누르면 SearchActivity로 넘어감
 
+            if(searchText.equals("")) Log.d(TAG,"null")
+            else{
+                val mAdapter : RecyclerView.Adapter<*> = SearchAdapter(DBLists!!)//searchAdapter의 searchList에 데이터베이스의 모든 레시피를 보내 filter에서 검색을 돌리도록하는것
+                (mAdapter as SearchAdapter).filter(searchText) //searchText는 searchAdapter의 filter의 searchText가되어 검색어가된다.
+                adapterCatchLists = (mAdapter).returnRecipe()  //adapterCatchList로 searchAdapter에서 나온 결과리스트를 받아옴
+                nextIntent.putExtra("sendList", adapterCatchLists)
+                startActivity(nextIntent)
+            }
         })
         db.collection("recipe")
             .get()
             .addOnSuccessListener{result->
-                firstNameList = ArrayList()
-                firstSearchList = ArrayList()
+                DBLists = ArrayList()
                 for(document in result){
-                    firstNameList!!.add(
-                        NameInfo(
-                            document.id,
-                            document.data["name"].toString(),
-                            document.data["ingredient"].toString(),
-                            document.data["picture"].toString()
-                        )
-                    )
+                    DBLists!!.add(NameInfo(document.id, document.data["name"].toString(), document.data["ingredient"].toString(), document.data["picture"].toString()))
                 }
             }
-
     }
 }
